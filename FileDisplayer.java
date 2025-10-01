f2f51db570b7d2bd3913a5b875c6a74412788a8e
Peru Gangoiti <p.gangoitilarranaga@student.maastrichtuniversity.nl>
2025-10-01 14:48:22 +0200

    added a new array that reads and saves all the course names, also fixed some bugs and cleaned code

--- FileDisplayer.java
+++ FileDisplayer.java
@@ -4,20 +4,17 @@ import java.util.Scanner;
 public class FileDisplayer {
 
     public static void main(String[] args) {
-	
 
         try {
             String fileName = "GraduateGrades.csv"; // The name of the file you want to read
             File file=new File(fileName); // Creating a File object so we can create code that reads the file
-            
 			
         	Scanner columnScanner = new Scanner(file); // This scanner is made to count the number of columns in the csv file
 			Scanner rowScanner = new Scanner(file); // This scanner is made to count the number of columns in the csv file
 			Scanner elementScanner = new Scanner(file); // This canner is made to record the elements from the csv file into the 2d array
 
-
 			int rows = -1; // This is the variable that stores the number of rows in the data, note that it starts as -1 because it removes the line that states the column names
-			int columns = 0; // this is the variable that stores the number of columns in the data
+			int columns = -1; // this is the variable that stores the number of columns in the data
 			int linesDone = 0; // This is the variable that keeps track of how many lines have been recorded into the array
 
             String line = columnScanner.nextLine(); // This is a variable that stores the current line which the column scanner is reading through
@@ -38,22 +35,23 @@ public class FileDisplayer {
 			}
 
 			double[][] GraduateGradesArray = new double[rows][columns]; // This is the 2D array that we create, it is an array of doubles because the code given to us mostly uses doubles
+			String[] CourseNamesArray = new String[columns]; // This is the array that stores the course names according to their courseID
             
 			// The following while loop fills the array with the grades for each course for each student
-			elementScanner.nextLine();
             while (elementScanner.hasNextLine() && linesDone < 50) {
             	line = elementScanner.nextLine();
             	lineScanner = new Scanner(line);
                 lineScanner.useDelimiter(",");
 				int j = 0;
+				int z = 0;
+				lineScanner.next();
             	while (lineScanner.hasNext()) {
-					if (lineScanner.hasNextInt()) {
-						lineScanner.next();
-					} else if (lineScanner.hasNextDouble()) {
-						GraduateGradesArray[linesDone][j] = lineScanner.nextDouble();
+					if (lineScanner.hasNextDouble()) {
+						GraduateGradesArray[linesDone-1][j] = lineScanner.nextDouble();
 						j++;
             		} else {
-            			lineScanner.next();
+            			CourseNamesArray[z] = lineScanner.next();
+						z++;
             		}
             	}
 				linesDone++;
@@ -62,21 +60,24 @@ public class FileDisplayer {
             elementScanner.close();
 
 			// All the code above this comment is made to prepare and fill the 2d array, from this point on you can use the 2d array for your tasks
-			// Below are instructions for using the 2d array
+			// Below are instructions for using the arrays
 			// The 2d array is exactly like a table, where you get the information for a specific element in the array by putting the numbers for rows and columns like so
 			// [rows][columns]
-			// In our data, the rows are the student IDs while the columns are the different course grades
+			// In our data, the rows are the student IDs while the columns are the course IDs
 			// The student ID can be used directly but the courseID is not referred to by name but by position
 			// The 0th course is "Cryogenic Physics", the 1st course is "Evolutionary Dynamics", the 2nd course is "Dark Energy", and so on
+			// Below you can see how the Course IDs can be used to get the Course name using the CourseNamesArray
+			System.out.println(CourseNamesArray[0] + ", " + CourseNamesArray[1] + ", " + CourseNamesArray[2]);
+			// Below is the format for getting information from the 2d array
 			// [studentID][courseID]
 			// For example [0][0] is the studentID 0 and the course 0, so it returns the number 8 which is the grade for "Cryogenic Physics" for the studentID 0
-			System.out.println(GraduateGradesArray[0][0]);
-			// Another example is [3][9] which would be 8 which is the grade for "Pulsar Spectroscopy" for studentID 3
-			System.out.println(GraduateGradesArray[3][9]);
-			// In the CSV there are no grades for "Signals" so all of them are automatically assigned a 0
-			// Remember that "Signals" is course number 36 in the array because arrays count from 0
-			System.out.println(GraduateGradesArray[0][36]);
-			// Write your code in between this comment and the next
+			System.out.println(CourseNamesArray[0] + " for student 0: " + GraduateGradesArray[0][0]);
+			// Another example is [5][13] which would be 10 which is the grade for "Warp Field Theory" for studentID 5
+			System.out.println(CourseNamesArray[13] + " for student 5: " + GraduateGradesArray[5][13]);
+			// When searching through the data, you will probably use a loop with an if statement inside, when your code does finds the CourseID youre looking for,
+			// you can get the name of the course with the CourseNamesArray by inputting that CourseID into the array
+			// Write your main method code in between this comment and the next
+
 
 
 
@@ -87,4 +88,8 @@ public class FileDisplayer {
             ex.printStackTrace();
         }
     }
+
+
+	// Your new method should start here
+	
 }
